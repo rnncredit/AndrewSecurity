f69c418e671b80a196f9b73c45e02026f230985e
Narender Bombili <writeme.narender@gmail.com>
2017-09-16 18:17:53 +0530

    comments added for heroku port changes

--- server/config/express.js
+++ server/config/express.js
@@ -1,6 +1,11 @@
 const express = require('express');
 var app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+
+//For Heroku app deployement, it will by default will assign port number to the
+// environment variable process.env.PORT
+
+//Remove the process.env.PORT env variable from config.js  for heroku deployment
 
 app.listen(port, ()=>{
     
